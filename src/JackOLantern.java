58f887bd05d24fb21c38743fec06953b0c401f6e
BT_1N3_17 <pyu6253@bths.edu>
2018-10-26 10:17:47 -0400

    pumkin

--- src/JackOLantern.java
+++ src/JackOLantern.java
@@ -1,3 +1,28 @@
-public class JackOLantern {
-    
+public class JackOLantern extends LanternRunner {
+    private String faceFeatures[][];
+    public JackOLantern(String[][] faceFeatures)
+    {
+        this.faceFeatures = faceFeatures;
+    }
+    public void edit(String Replacing,int row,int column)
+    {
+
+    }
+    public void fill(String face)
+    {
+
+    }
+    public String toString()
+    {
+        String face = "";
+        for(int i = 0;i < faceFeatures.length;i++)
+        {
+            for(int x = 0;x < faceFeatures[i].length;i++)
+            {
+                face += faceFeatures[i][x];
+                face += "\n";
+            }
+        }
+        return face;
+    }
 }
--- src/LanternRunner.java
+++ src/LanternRunner.java
@@ -1,2 +1,9 @@
-public class LanternRunner {
+public class LanternRunner{
+    public static void main(String[]args)
+    {
+        JackOLantern face = new JackOLantern(faceFeatures);
+        String[][] faceFeatures = {{"00000000"},{"00000000"},{"00000000"},{"00000000"},{"00000000"}, {"00000000"},
+                {"00000000"}
+                .toString();
+    }
 }
